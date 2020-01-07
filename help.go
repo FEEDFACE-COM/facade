9536184d5cc33be1f2bac49139e54a144568dea7
Folkert Saathoff <folkert@feedface.com>
2020-01-07 22:10:39 +0100

    refactor config

---
+++ help.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	facade "./facade"
+)
+
+func ShowHelp() {
+	cmds := []string{}
+	if RENDERER_AVAILABLE {
+		for _, c := range []Command{READ, RECV} {
+			cmds = append(cmds, string(c))
+		}
+	}
+	for _, c := range []Command{PIPE, CONF, EXEC, INFO} {
+		cmds = append(cmds, string(c))
+	}
+
+	ShowVersion()
+	fmt.Fprintf(os.Stderr, "\nUsage:\n")
+	fmt.Fprintf(os.Stderr, "  %s [flags]  %s\n", BUILD_NAME, strings.Join(cmds, " | "))
+	ShowCommands()
+	fmt.Fprintf(os.Stderr, "\nFlags:\n")
+	flag.VisitAll(func(f *flag.Flag) {
+		name := f.Name
+		if f.DefValue != "false" && f.DefValue != "true" {
+			name = f.Name + "=" + f.DefValue
+		}
+		fmt.Fprintf(os.Stderr, "  -%-24s %-24s\n", name, f.Usage)
+	})
+	fmt.Fprintf(os.Stderr, "\n")
+}
+
+func ShowHelpMode(cmd Command, mode facade.Mode, flags flag.FlagSet) {
+	ShowVersion()
+	fmt.Fprintf(os.Stderr, "\nUsage:\n")
+	fmt.Fprintf(os.Stderr, "  %s %s %s [flags]\n", BUILD_NAME, cmd, strings.ToLower(mode.String()))
+
+	fmt.Fprintf(os.Stderr, "\nFlags:\n")
+
+	fmt.Fprintf(os.Stderr, "%s", facade.FontDefaults.Help())
+	fmt.Fprintf(os.Stderr, "%s", facade.MaskDefaults.Help())
+	fmt.Fprintf(os.Stderr, "%s", facade.CameraDefaults.Help())
+	fmt.Fprintf(os.Stderr, "%s", facade.Defaults.Help())
+
+	switch mode {
+	case facade.Mode_LINE:
+		fmt.Fprintf(os.Stderr, "%s", facade.LineDefaults.Help())
+	case facade.Mode_TERM:
+		fmt.Fprintf(os.Stderr, "%s", facade.TermDefaults.Help())
+	}
+
+	fmt.Fprintf(os.Stderr, "\n")
+}
+
+func ShowHelpCommand(cmd Command, flags flag.FlagSet) {
+	var modes []string
+	modes = append(modes, strings.ToLower(facade.Mode_TERM.String()))
+	modes = append(modes, strings.ToLower(facade.Mode_LINE.String()))
+
+	ShowVersion()
+	fmt.Fprintf(os.Stderr, "\nUsage:\n")
+	switch cmd {
+	case INFO:
+		fmt.Fprintf(os.Stderr, "  %s %s [flags]\n", BUILD_NAME, cmd)
+	default:
+		fmt.Fprintf(os.Stderr, "  %s %s [flags]  %s\n", BUILD_NAME, cmd, strings.Join(modes, " | "))
+		ShowModes()
+	}
+
+	fmt.Fprintf(os.Stderr, "\nFlags:\n")
+	flags.VisitAll(func(f *flag.Flag) {
+		name := f.Name
+		if f.DefValue != "false" && f.DefValue != "true" {
+			name = f.Name + "=" + f.DefValue
+		}
+		fmt.Fprintf(os.Stderr, "  -%-24s %-24s\n", name, f.Usage)
+	})
+	fmt.Fprintf(os.Stderr, "\n")
+}
+
+func ShowCommands() {
+	fmt.Fprintf(os.Stderr, "\nCommands:\n")
+	if RENDERER_AVAILABLE {
+		fmt.Fprintf(os.Stderr, "%6s     %s\n", READ, "read text from stdin and render")
+		fmt.Fprintf(os.Stderr, "%6s     %s\n", RECV, "receive text from client and render ")
+	}
+	fmt.Fprintf(os.Stderr, "%6s     %s\n", PIPE, "read text from stdin and send to server")
+	fmt.Fprintf(os.Stderr, "%6s     %s\n", CONF, "change server configuration")
+	fmt.Fprintf(os.Stderr, "%6s     %s\n", EXEC, "execute command and send stdio to server")
+	fmt.Fprintf(os.Stderr, "%6s     %s\n", INFO, "show available shaders and fonts of server ")
+}
+
+func ShowModes() {
+	fmt.Fprintf(os.Stderr, "\nModes:\n")
+	fmt.Fprintf(os.Stderr, "%6s     %s\n", strings.ToLower(facade.Mode_TERM.String()), "text terminal")
+	fmt.Fprintf(os.Stderr, "%6s     %s\n", strings.ToLower(facade.Mode_LINE.String()), "line scroller")
+}
+
+func ShowAssets() {
+
+	//FIXME
+	fmt.Fprintf(os.Stderr, InfoAssets(nil, nil))
+}
+
+func InfoAssets(shaders, fonts []string) string {
+	ret := ""
+
+	for _, prefix := range []string{"grid/"} {
+		for _, suffix := range []string{".vert", ".frag"} {
+			tmp := strings.TrimSuffix(prefix, "/") + " -" + strings.TrimPrefix(suffix, ".")
+			ret += fmt.Sprintf("\n%12s= ", tmp)
+			for _, shader := range shaders {
+				if strings.HasPrefix(shader, prefix) && strings.HasSuffix(shader, suffix) {
+					ret += strings.TrimSuffix(strings.TrimPrefix(shader, prefix), suffix)
+					ret += " "
+				}
+
+			}
+		}
+	}
+
+	ret += fmt.Sprintf("\n%12s= ", "-mask")
+	for _, shader := range shaders {
+		if strings.HasPrefix(shader, "mask/") && strings.HasSuffix(shader, "frag") {
+			ret += strings.TrimSuffix(strings.TrimPrefix(shader, "mask/"), ".frag")
+			ret += " "
+		}
+	}
+
+	ret += fmt.Sprintf("\n%12s= ", "-font")
+	for _, font := range fonts {
+		ret += font
+		ret += " "
+	}
+	ret += "\n"
+	return ret
+}
+
+func ShowVersion() { fmt.Fprintf(os.Stderr, InfoVersion()) }
+
+func InfoVersion() string {
+	ret := ""
+	ret += AUTHOR
+	ret += fmt.Sprintf("\n%s version %s for %s built %s\n", BUILD_NAME, BUILD_VERSION, BUILD_PLATFORM, BUILD_DATE)
+	return ret
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"strings"
@@ -93,6 +94,8 @@ func main() {
 
 	for _, cmd := range commands {
 		flags[cmd] = flag.NewFlagSet(string(cmd), flag.ExitOnError)
+		flags[cmd].Usage = ShowHelp
+		flags[cmd].SetOutput(bufio.NewWriter(nil))
 	}
 
 	for _, cmd := range []Command{PIPE, CONF, EXEC, INFO} {
@@ -180,7 +183,8 @@ func main() {
 		}
 
 		modeFlags = flag.NewFlagSet("term", flag.ExitOnError)
-		modeFlags.Usage = func() { ShowHelpMode(config.Mode, EXEC) }
+		modeFlags.SetOutput(bufio.NewWriter(nil))
+		modeFlags.Usage = func() { ShowHelpMode(EXEC, config.Mode, *modeFlags) }
 
 		config.AddFlags(modeFlags)
 		modeFlags.Parse(args)
@@ -189,7 +193,7 @@ func main() {
 		args = modeFlags.Args()
 		log.Debug("got flags %v", args)
 		if len(args) <= 0 {
-			ShowHelpMode(facade.Mode_TERM, EXEC)
+			ShowHelpMode(EXEC, facade.Mode_TERM, *modeFlags)
 			os.Exit(-2)
 		}
 
@@ -197,9 +201,9 @@ func main() {
 		args = args[1:]
 
 	case READ, RECV, PIPE, CONF, TEST:
-		// parse mode, if given
 		args = flags[cmd].Args()
 
+		// parse mode, if given
 		if cmd != INFO && len(args) > 0 {
 
 			switch strings.ToUpper(args[0]) {
@@ -226,12 +230,11 @@ func main() {
 
 			}
 
-			args = args[1:]
 			modeFlags = flag.NewFlagSet(strings.ToLower(config.Mode.String()), flag.ExitOnError)
-			modeFlags.Usage = func() { ShowHelpMode(config.Mode, cmd) }
-			modeFlags.Parse(args)
+			modeFlags.Usage = func() { ShowHelpMode(cmd, config.Mode, *modeFlags) }
+			modeFlags.SetOutput(bufio.NewWriter(nil))
+			config.AddFlags(modeFlags)
 			config.VisitFlags(modeFlags)
-
 		}
 
 	case INFO:
@@ -247,6 +250,7 @@ func main() {
 
 			// query remote host
 			flags[INFO].Usage = func() { ShowHelpCommand(INFO, *flags[INFO]) }
+			flags[INFO].SetOutput(bufio.NewWriter(nil))
 			flags[INFO].Parse(flag.Args()[1:])
 
 		}
@@ -317,7 +321,7 @@ func main() {
 
 	case CONF:
 		if config == nil {
-			ShowHelpMode(facade.Mode_DRAFT, cmd)
+			ShowHelpMode(cmd, facade.Mode_DRAFT, *modeFlags)
 			os.Exit(-1)
 		}
 		log.Debug("configure %s", config.Desc())
@@ -412,146 +416,4 @@ func main() {
 	}
 
 	os.Exit(0)
-}
-
-func ShowHelpMode(mode facade.Mode, cmd Command) {
-	ShowVersion()
-	fmt.Fprintf(os.Stderr, "\nUsage:\n")
-	fmt.Fprintf(os.Stderr, "  %s %s %s [flags]\n", BUILD_NAME, cmd, strings.ToLower(mode.String()))
-
-	fmt.Fprintf(os.Stderr, "\nFlags:\n")
-
-	fmt.Fprintf(os.Stderr, "%s", facade.FontDefaults.Help())
-	fmt.Fprintf(os.Stderr, "%s", facade.MaskDefaults.Help())
-	fmt.Fprintf(os.Stderr, "%s", facade.CameraDefaults.Help())
-	fmt.Fprintf(os.Stderr, "%s", facade.Defaults.Help())
-
-	switch mode {
-	case facade.Mode_LINE:
-		fmt.Fprintf(os.Stderr, "%s", facade.LineDefaults.Help())
-	case facade.Mode_TERM:
-		fmt.Fprintf(os.Stderr, "%s", facade.TermDefaults.Help())
-	}
-
-	fmt.Fprintf(os.Stderr, "\n")
-}
-
-func ShowHelpCommand(cmd Command, flags flag.FlagSet) {
-	var modes []string
-	modes = append(modes, strings.ToLower(facade.Mode_TERM.String()))
-	modes = append(modes, strings.ToLower(facade.Mode_LINE.String()))
-
-	ShowVersion()
-	fmt.Fprintf(os.Stderr, "\nUsage:\n")
-	switch cmd {
-	case INFO:
-		fmt.Fprintf(os.Stderr, "  %s %s [flags]\n", BUILD_NAME, cmd)
-	default:
-		fmt.Fprintf(os.Stderr, "  %s %s [flags]  %s\n", BUILD_NAME, cmd, strings.Join(modes, " | "))
-		ShowModes()
-	}
-
-	fmt.Fprintf(os.Stderr, "\nFlags:\n")
-	flags.VisitAll(func(f *flag.Flag) {
-		name := f.Name
-		if f.DefValue != "false" && f.DefValue != "true" {
-			name = f.Name + "=" + f.DefValue
-		}
-		fmt.Fprintf(os.Stderr, "  -%-24s %-24s\n", name, f.Usage)
-	})
-	fmt.Fprintf(os.Stderr, "\n")
-}
-
-func ShowCommands() {
-	fmt.Fprintf(os.Stderr, "\nCommands:\n")
-	if RENDERER_AVAILABLE {
-		fmt.Fprintf(os.Stderr, "%6s     %s\n", READ, "read text from stdin and render")
-		fmt.Fprintf(os.Stderr, "%6s     %s\n", RECV, "receive text from client and render ")
-	}
-	fmt.Fprintf(os.Stderr, "%6s     %s\n", PIPE, "read text from stdin and send to server")
-	fmt.Fprintf(os.Stderr, "%6s     %s\n", CONF, "change server configuration")
-	fmt.Fprintf(os.Stderr, "%6s     %s\n", EXEC, "execute command and send stdio to server")
-	fmt.Fprintf(os.Stderr, "%6s     %s\n", INFO, "show available shaders and fonts of server ")
-}
-
-func ShowModes() {
-	fmt.Fprintf(os.Stderr, "\nModes:\n")
-	fmt.Fprintf(os.Stderr, "%6s     %s\n", strings.ToLower(facade.Mode_TERM.String()), "text terminal")
-	fmt.Fprintf(os.Stderr, "%6s     %s\n", strings.ToLower(facade.Mode_LINE.String()), "line scroller")
-}
-
-func ShowHelp() {
-	cmds := []string{}
-	if RENDERER_AVAILABLE {
-		for _, c := range []Command{READ, RECV} {
-			cmds = append(cmds, string(c))
-		}
-	}
-	for _, c := range []Command{PIPE, CONF, EXEC, INFO} {
-		cmds = append(cmds, string(c))
-	}
-
-	ShowVersion()
-	fmt.Fprintf(os.Stderr, "\nUsage:\n")
-	fmt.Fprintf(os.Stderr, "  %s [flags]  %s\n", BUILD_NAME, strings.Join(cmds, " | "))
-	ShowCommands()
-	fmt.Fprintf(os.Stderr, "\nFlags:\n")
-	flag.VisitAll(func(f *flag.Flag) {
-		name := f.Name
-		if f.DefValue != "false" && f.DefValue != "true" {
-			name = f.Name + "=" + f.DefValue
-		}
-		fmt.Fprintf(os.Stderr, "  -%-24s %-24s\n", name, f.Usage)
-	})
-	fmt.Fprintf(os.Stderr, "\n")
-
-}
-
-func ShowAssets() {
-
-	//FIXME
-	fmt.Fprintf(os.Stderr, InfoAssets(nil, nil))
-}
-
-func InfoAssets(shaders, fonts []string) string {
-	ret := ""
-
-	for _, prefix := range []string{"grid/"} {
-		for _, suffix := range []string{".vert", ".frag"} {
-			tmp := strings.TrimSuffix(prefix, "/") + " -" + strings.TrimPrefix(suffix, ".")
-			ret += fmt.Sprintf("\n%12s= ", tmp)
-			for _, shader := range shaders {
-				if strings.HasPrefix(shader, prefix) && strings.HasSuffix(shader, suffix) {
-					ret += strings.TrimSuffix(strings.TrimPrefix(shader, prefix), suffix)
-					ret += " "
-				}
-
-			}
-		}
-	}
-
-	ret += fmt.Sprintf("\n%12s= ", "-mask")
-	for _, shader := range shaders {
-		if strings.HasPrefix(shader, "mask/") && strings.HasSuffix(shader, "frag") {
-			ret += strings.TrimSuffix(strings.TrimPrefix(shader, "mask/"), ".frag")
-			ret += " "
-		}
-	}
-
-	ret += fmt.Sprintf("\n%12s= ", "-font")
-	for _, font := range fonts {
-		ret += font
-		ret += " "
-	}
-	ret += "\n"
-	return ret
-}
-
-func ShowVersion() { fmt.Fprintf(os.Stderr, InfoVersion()) }
-
-func InfoVersion() string {
-	ret := ""
-	ret += AUTHOR
-	ret += fmt.Sprintf("\n%s version %s for %s built %s\n", BUILD_NAME, BUILD_VERSION, BUILD_PLATFORM, BUILD_DATE)
-	return ret
 }
